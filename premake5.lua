eaf2d79f4e33fcd02df08bdaa3e5e681eea7de33
djperrone <david_perrone@uri.edu>
2021-09-14 17:46:57 -0400

    starter

---
+++ premake5.lua
@@ -0,0 +1,86 @@
+workspace "csc406setup"
+	architecture "x32"
+	startproject "csc406setup"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- include directories relative to root folder (solution directory)
+
+IncludeDir = {}
+IncludeDir["glew"] = "csc406setup/vendor/glew/include/gl"
+IncludeDir["freeglut"] = "csc406setup/vendor/freeglut/include/gl"
+
+
+--include "csc406setup/vendor/glew/include"
+--include "csc406setup/vendor/freeglut/include"
+
+
+project "csc406setup"
+	location "csc406setup"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+	
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")	
+
+	-- pchheader "sapch.h"
+	-- pchsource "csc406setup/src/sapch.cpp"
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"	
+	}
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	
+	}
+	debugenvs { "PATH=%PATH%; %{prj.name}/vendor/freeglut/bin";}
+
+	includedirs
+	{
+		"%{prj.name}/src",					
+		"%{IncludeDir.glew}",
+		"%{IncludeDir.freeglut}",
+		
+	}
+
+	libdirs { 
+                "%{prj.name}/vendor/glew/lib",
+                "%{prj.name}/vendor/freeglut/lib",
+                
+             }
+
+	links
+	{
+		"glew32.lib",
+		"freeglut.lib",		
+		"opengl32.lib"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{			
+			--"GLFW_INCLUDE_NONE"		
+		}
+
+	filter "configurations:Debug"			
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
